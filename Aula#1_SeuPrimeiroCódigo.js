83b0038797e0a12d985120470a9e803a5dbf9780
Franciele B. de Oliveira <32960110+FranOlv9pt1br3uff@users.noreply.github.com>
2020-04-11 14:21:09 -0300

    Update Aula#1_SeuPrimeiroCódigo.js

--- Aula#1_SeuPrimeiroCódigo.js
+++ Aula#1_SeuPrimeiroCódigo.js
@@ -13,16 +13,17 @@ Com a informação da quantidade de batida que o coração da por dia,
 conseguimos calcular quantas batidas o coração da por ano já que sabemos quantos dias o ano tem. 
 Agora é só calcular quanto o coração já bateu durante toda nossa vida. */
 
-alert('Meu coraçao ja bateu' 25*75*60*24*365  'vezes!')
-
+alert('Meu coraçao ja bateu ' + 25*75*60*24*365 +' vezes!')
 //Exercícios
 
 //O Criador de galinha me falou o seguinte: "Comprei 15 galinhas por 300 reias" Quanto ele pagou por cada galinha?
-alert(300/15)
+alert('O criador de galinhas pagou R$'+ 300/15+' em cada galinha')
 
 //Maria foi a padaria e comprou 19 balas, dpois voltou a padaria e comprou mais 45. Quantas balas Maria tem agora?
-alert(45+19)
-      
+alert('Maria comprou '+(19+45)+' balas')
+
 //Para calcular a área de uma circunferência usamos a seguinte fórmula: area = pi x raio x raio. 
 //O valor de pi = 3.14, se o valor do meu raio é 2 qual a área da circunferência?
-alert(3.14*2*2)
+var pi = 3.14
+alert('A área da circunferência é '+(pi*2*2))
+
