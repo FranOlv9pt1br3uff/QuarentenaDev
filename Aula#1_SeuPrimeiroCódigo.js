e1db95e2670c3ab950a0952377fc5198b6a83f2f
Franciele B. de Oliveira <32960110+FranOlv9pt1br3uff@users.noreply.github.com>
2020-04-11 14:09:45 -0300

    Update Aula#1_SeuPrimeiroCódigo.js

--- Aula#1_SeuPrimeiroCódigo.js
+++ Aula#1_SeuPrimeiroCódigo.js
@@ -1,15 +1,28 @@
-Quantos dias você já viveu?
+// Quantos dias você já viveu?
 
 alert('Eu ja vivi' 25*365 'dias')
 
-Desafio
+//Desafio
 
-cálculo de batimentos cárdiacos
+/*Cálculo de batimentos cárdiacos
+O nosso coração bate uma média de 75 vezes por minuto aproximadamente. 
+Sabemos que 1 hora tem 60 minutos.
+Com essa informação conseguimos descobrir quantas batidas por hora o coração dá.
+Agora que sabemos as batidas por hora, conseguimos saber quantas batidas ele da por dia já que sabemos quantas horas tem o dia.
+Com a informação da quantidade de batida que o coração da por dia, 
+conseguimos calcular quantas batidas o coração da por ano já que sabemos quantos dias o ano tem. 
+Agora é só calcular quanto o coração já bateu durante toda nossa vida. */
 
-Exercícios
+alert('Meu coraçao ja bateu' 25*75*60*24*365  'vezes!')
 
-O Criador de galinha me falou o seguinte: "Comprei 15 galinhas por 300 reias" Quanto ele pagou por cada galinha?
+//Exercícios
 
-Maria foi a padaria e comprou 19 balas, dpois voltou a padaria e comprou mais 45. Quantas balas Maria tem agora?
+//O Criador de galinha me falou o seguinte: "Comprei 15 galinhas por 300 reias" Quanto ele pagou por cada galinha?
+alert(300/15)
 
-Para calcular a área de uma circunferência usamos a seguinte fórmula: area = pi x raio x raio. O valor de pi = 3.14, se o valor do meu raio é 2 qual a área da circunferência?
+//Maria foi a padaria e comprou 19 balas, dpois voltou a padaria e comprou mais 45. Quantas balas Maria tem agora?
+alert(45+19)
+      
+//Para calcular a área de uma circunferência usamos a seguinte fórmula: area = pi x raio x raio. 
+//O valor de pi = 3.14, se o valor do meu raio é 2 qual a área da circunferência?
+alert(3.14*2*2)
